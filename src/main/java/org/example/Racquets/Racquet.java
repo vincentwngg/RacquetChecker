270763de8531f0c01d4a00d12118e609ba8888d4
Vincent Wang <vdubs17@iastate.edu>
2025-05-20 17:05:38 -0500

    Working on racquet classes to store data.

---
+++ src/main/java/org/example/Racquets/Racquet.java
@@ -0,0 +1,104 @@
+package org.example.Racquets;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Racquet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String brand;
+    private String model;
+    private int ebayId;
+    private String imageUrl;
+    private BigDecimal price;
+    private String specs;
+
+    public Racquet(){
+
+    }
+
+    public Racquet(String brand, String model, int ebayId, String imageUrl, BigDecimal price, String specs){
+        this.brand = brand;
+        this.model = model;
+        this.ebayId = ebayId;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.specs = specs;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public String getSpecs() {
+        return specs;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEbayId(int ebayId) {
+        this.ebayId = ebayId;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setSpecs(String specs) {
+        this.specs = specs;
+    }
+
+    public int getEbayId() {
+        return ebayId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    @Override
+    public String toString() {
+        return "Racquet{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", racquetId=" + ebayId +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", price='" + price + '\'' +
+                ", specs='" + specs + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/Racquets/RacquetController.java
@@ -0,0 +1,4 @@
+package org.example.Racquets;
+
+public class RacquetController {
+}
