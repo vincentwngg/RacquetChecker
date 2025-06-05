027618c6a046b2adb1910bbf4a709e300dee6ee1
Vincent Wang <vdubs17@iastate.edu>
2025-06-04 22:36:58 -0500

    Working on racquet classes to store data.

--- src/main/java/org/example/Racquets/RacquetController.java
+++ src/main/java/org/example/Racquets/RacquetController.java
@@ -1,4 +1,57 @@
 package org.example.Racquets;
 
+import org.example.Users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class RacquetController {
+
+    private static final String success = "{\"message\":\"success\"}";
+    private static final String failure = "{\"message\":\"failure\"}";
+
+    @Autowired
+    RacquetRepository racquetRepository;
+
+    public RacquetController(RacquetRepository racquetRepository){
+        this.racquetRepository = racquetRepository;
+    }
+
+    @GetMapping(path = "/racquets")
+    public ResponseEntity<List<Racquet>> getAllRacquets(){
+        return ResponseEntity.ok(racquetRepository.findAll());
+    }
+
+    @PostMapping(path = "/racquets/add")
+    public ResponseEntity<String> addRacquet(@RequestBody Racquet racquet){
+        if(racquet == null){
+            return ResponseEntity.badRequest().body(failure);
+        }
+
+        try{
+            racquetRepository.save(racquet);
+
+            return ResponseEntity.ok(success);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(failure);
+        }
+    }
+
+    @DeleteMapping(path = "/racquets/delete/{id}")
+    public ResponseEntity<String> deleteRacquet(@PathVariable Long id){
+        if(racquetRepository.findById(id).isPresent()){
+            racquetRepository.deleteById(id);
+            return ResponseEntity.ok(success);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(failure);
+    }
+
+
+
+
+
 }
